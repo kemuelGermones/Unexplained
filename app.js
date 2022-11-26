218ce112d1d39e712c1dd1cd01551b748c904ff4
Kemuel Germones <kemuelgermones@gmail.com>
2022-11-26 15:10:49 +0800

    Fix errors on some page

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const MongoStore = require("connect-mongo");
 
 // Mongoose Connection
 
-const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/anomalies";
+const dbUrl = process.env.DB_URL || "mongodb://127.0.0.1:27017/anomalies";
 mongoose.connect(dbUrl);
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
